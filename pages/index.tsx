288da080862482e977bb66257dee205011a010f3
tyri1 <iherewithmyheart@gmail.com>
2023-02-13 18:07:11 +0800

    chore: init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+const Hi = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >{`Hi, I'm Tyler Rio`}</div>
+  )
+}
+
+export default Hi
