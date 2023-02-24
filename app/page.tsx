a99649231c4c323bb65e22e242282f475488c09e
tyri1 <iherewithmyheart@gmail.com>
2023-02-24 18:47:28 +0800

    fix: ci

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import { useEffect } from 'react'
+
 export const metadata = {
   title: 'Tyler Roi',
   description: 'tyr<hi@tyr.im>',
 }
 
-const redirect = () => {
-  location.href =
-    'https://tyri.notion.site/tyr-da3a1dce8823418eb4e80befc0491714'
+const Hi = () => {
+  useEffect(() => {
+    location.href =
+      'https://tyri.notion.site/tyr-da3a1dce8823418eb4e80befc0491714'
+  }, [])
+
+  return (
+    <div className='flex h-screen items-center justify-center'>{`Hi, I'm Tyler Roi`}</div>
+  )
 }
 
-export default redirect
+export default Hi
