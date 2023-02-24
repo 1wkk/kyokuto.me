5f1a30f9032ba3012ed9011ed55e0520d2e860b4
tyri1 <iherewithmyheart@gmail.com>
2023-02-24 18:41:46 +0800

    feat: redirect to Notion CMS

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,13 @@ import '@/styles/tailwind.css'
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 const Layout = ({ children }: { children: ReactNode }) => (
-  <html lang='en'>
-    <body className={`font-sans ${inter.variable}`}>{children}</body>
+  <html
+    lang='en'
+    className={`bg-white text-black dark:bg-black dark:text-white ${inter.className}`}
+  >
+    <body className='mx-4 mb-40 mt-8 flex max-w-4xl flex-col font-sans antialiased'>
+      {children}
+    </body>
   </html>
 )
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-const Hi = () => {
-  return (
-    <div className='flex h-screen items-center justify-center'>{`Hi, I'm Tyler Rio`}</div>
-  )
-}
+'use client'
 
 export const metadata = {
-  title: 'Tyler Rio',
+  title: 'Tyler Roi',
   description: 'tyr<hi@tyr.im>',
 }
 
-export default Hi
+const redirect = () => {
+  location.href =
+    'https://tyri.notion.site/tyr-da3a1dce8823418eb4e80befc0491714'
+}
+
+export default redirect
